02334baaa2985f23de8f3b8849e73f831e85accf
muhammadatifarif <71638341+muhammadatifarif@users.noreply.github.com>
2025-08-30 18:07:22 +0400

    first commit

--- app/src/main/java/com/mak/androidmvi/MainActivity.kt
+++ app/src/main/java/com/mak/androidmvi/MainActivity.kt
@@ -1,42 +1,19 @@
 package com.mak.androidmvi
 
 import android.os.Bundle
+import android.window.SplashScreen
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Email
-import androidx.compose.material.icons.filled.Home
-import androidx.compose.material.icons.filled.Search
-import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material.icons.outlined.Email
-import androidx.compose.material.icons.outlined.Home
-import androidx.compose.material.icons.outlined.Search
-import androidx.compose.material.icons.outlined.Settings
-import androidx.compose.material3.Badge
-import androidx.compose.material3.BadgedBox
-import androidx.compose.material3.Icon
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.NavigationBar
-import androidx.compose.material3.NavigationBarItem
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.SnackbarHostState
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.rememberCoroutineScope
-import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
@@ -48,11 +25,10 @@ import androidx.core.view.WindowCompat
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
-import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.mak.androidmvi.core.manager.EventManager
-import com.mak.androidmvi.core.model.BottomNavigationItem
 import com.mak.androidmvi.core.navigation.RootNavigationGraph
+import com.mak.androidmvi.ui.screens.splash.SplashViewModel
 import com.mak.androidmvi.ui.theme.AndroidMviTheme
 import kotlinx.coroutines.launch
 
@@ -60,10 +36,17 @@ class MainActivity : ComponentActivity() {
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        installSplashScreen() // Keep the splash screen visible
+        val splashScreen = installSplashScreen()
+        val splashViewModel = SplashViewModel()
+        // Keep the splash screen on display until isLoading is false
+        splashScreen.setKeepOnScreenCondition { splashViewModel.uiState.value.isLoading }
+
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        installSplashScreen()
         WindowCompat.setDecorFitsSystemWindows(window, false)
+
+
 
         setContent {
 
