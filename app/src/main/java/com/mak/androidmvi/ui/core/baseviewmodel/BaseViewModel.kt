fb8de48235d2e0cc6635fe1b400448390b8ef35a
muhammadatifarif <71638341+muhammadatifarif@users.noreply.github.com>
2025-09-28 22:40:57 +0400

    optimized

--- app/src/main/java/com/mak/androidmvi/ui/core/baseviewmodel/BaseViewModel.kt
+++ app/src/main/java/com/mak/androidmvi/ui/core/baseviewmodel/BaseViewModel.kt
@@ -3,18 +3,37 @@ package com.mak.androidmvi.ui.core.baseviewmodel
 import android.app.Application
 import androidx.lifecycle.AndroidViewModel
 import androidx.lifecycle.DefaultLifecycleObserver
+import androidx.lifecycle.LifecycleOwner
 import com.mak.androidmvi.ui.core.sharedviewmodel.data.SharedViewData
 
 abstract class BaseViewModel<S : SharedViewData>(
-    context: Application
-) : AndroidViewModel(context), DefaultLifecycleObserver {
+    application: Application
+) : AndroidViewModel(application), DefaultLifecycleObserver {
 
-    var sharedData: S? = null
+    private var _sharedData: S? = null
+    val sharedData: S?
+        get() = _sharedData
 
     fun updateSharedData(sharedData: S?) {
-        this.sharedData = sharedData
-        onSharedDataUpdated()
+        _sharedData = sharedData
+        onSharedDataUpdated(sharedData)
     }
 
-    open fun onSharedDataUpdated() {}
+    open fun onSharedDataUpdated(sharedData: S?) {}
+
+    override fun onDestroy(owner: LifecycleOwner) {
+        // Clear data when NavGraph lifecycle is destroyed
+        _sharedData?.onCleared()
+        _sharedData?.clear()
+        _sharedData = null
+        super.onDestroy(owner)
+    }
+
+    override fun onCleared() {
+        // When ViewModel is removed from memory
+        _sharedData?.onCleared()
+        _sharedData?.clear()
+        _sharedData = null
+        super.onCleared()
+    }
 }
